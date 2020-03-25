829fa5cf7c706689faf870fb4c0ae0937bf55e27
Diego Alzate <diegoff@gmail.com>
2020-03-25 11:40:33 -0500

    Tarea 25-03-2020

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
 import React from 'react';
 import './App.css';
+import uuid from 'uuid-random';
 
 function TaskList(props) {
   return props.tasks.map((task, index) => (
-    <p
-      onClick={props.onClick}
-      id={index + 1}
-      name={task}
-      className={props.className}
-    >
-      {task}
+    <p key={uuid()} onClick={props.onClick} id={index + 1} name={task.title}>
+      {task.title}-{task.description}
     </p>
   ));
 }
 
 class App extends React.Component {
   state = {
-    tasks: [
-      'task1',
-      'task2',
-      'task3',
-      'task4',
-      'task5',
-      'task6',
-      'task7',
-      'task8',
-      'task9',
-      'task10'
-    ],
-    completedTasks: ['completed']
+    tasks: [],
+    completedTasks: [],
+    toDo: '',
+    description: ''
   };
 
   clickTasks = event => {
@@ -57,17 +44,48 @@ class App extends React.Component {
         .slice(0, index - 1)
         .concat(prevState.completedTasks.slice(index))
     }));
-    // const newTask = this.state.completedTasks.splice(
-    //   event.currentTarget.id - 1,
-    //   1
-    // )[0];
-    // this.state.tasks.push(newTask);
-    // this.setState(prevState => ({}));
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const newTask = {
+      id: uuid(),
+      title: this.state.toDo,
+      description: this.state.description
+    };
+    this.setState(prevState => ({
+      tasks: prevState.tasks.concat(newTask)
+    }));
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   };
 
   render() {
     return (
       <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <label>Tarea</label>
+          <input
+            type="Text"
+            name="toDo"
+            id="toDo"
+            value={this.toDo}
+            onChange={this.handleChange}
+          />
+          <label>Descripción</label>
+          <input
+            type="Text"
+            name="description"
+            id="description"
+            value={this.description}
+            onChange={this.handleChange}
+          />
+          <button>Crear</button>
+        </form>
         <h2>Tasks</h2>
         <TaskList onClick={this.clickTasks} tasks={this.state.tasks} />
         <h2>Completed tasks</h2>
